22c346dbd890ba4e7cb44f786d46c1521866170d
data <K1NGKAOS100@Gmail.com>
2020-10-26 08:52:33 -0700

    Added ConnectCode and InLobby global strings

--- app/src/main/java/com/data/linkup/Globals.java
+++ app/src/main/java/com/data/linkup/Globals.java
@@ -3,5 +3,8 @@ package com.data.linkup;
 public class Globals
 {
     public static String HwidString;
+    public static String ConnectCode;
+
     public static Boolean IsVerified;
+    public static Boolean InLobby;
 }
