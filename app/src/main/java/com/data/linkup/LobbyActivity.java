603112ee1e2c29a8387e9e685c0b4b4f188ea84b
data <K1NGKAOS100@Gmail.com>
2020-10-26 08:54:41 -0700

    Made lobby turn InLobby to true to indicate that user is viewing lobby activity, while loop checks for when ConnectCode is not null and sets the text of codeView, and attempts to center it (centering seems to be broken).
    codeViewClick is called whenever the user clicks the code, it copies to clipboard and sends a toast to the user indicating that it did.

--- app/src/main/java/com/data/linkup/LobbyActivity.java
+++ app/src/main/java/com/data/linkup/LobbyActivity.java
@@ -1,18 +1,43 @@
 package com.data.linkup;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LobbyActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
 
+        Globals.InLobby = true;
 
+        while(true) {
+            if(Globals.ConnectCode != null) {
+                TextView codeView = (TextView)findViewById(R.id.codeView);
+                codeView.setText(Globals.ConnectCode);
+                codeView.setGravity(Gravity.CENTER);
+                break;
+            }
+        }
+    }
+
+    public void codeViewClick(View v) {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("test","test");
+        clipboard.setPrimaryClip(clip);
+
+        Toast toast = Toast.makeText(this , "Saved to clipboard", Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 1200);
+        toast.show();
     }
 }
