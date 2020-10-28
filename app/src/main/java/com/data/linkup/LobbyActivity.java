89276f5fe22043819752ba09ca85dd3de445e5bb
data <K1NGKAOS100@Gmail.com>
2020-10-28 08:59:16 -0700

    Lobby now checks if Globals.GotConnectCode is true, forcing it to wait for ConnThread to aquire the ConnectCode from the server first, preventing race condition.
    Function "onBackPressed()" overridden to assign Globals.InLobby to false, to indicate we are no longer in lobby.

--- app/src/main/java/com/data/linkup/LobbyActivity.java
+++ app/src/main/java/com/data/linkup/LobbyActivity.java
@@ -4,6 +4,7 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
@@ -19,16 +20,24 @@ public class LobbyActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
 
+        Log.d("onCreate_Lobby", "Ran onCreate()");
         Globals.InLobby = true;
 
         while(true) {
-            if(Globals.ConnectCode != null) {
+            if(Globals.ConnectCode != null && Globals.GotConnectCode == true) {
                 TextView codeView = (TextView)findViewById(R.id.codeView);
                 codeView.setText(Globals.ConnectCode);
                 codeView.setGravity(Gravity.CENTER);
                 break;
             }
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        Log.d("LobbyActivity","Back pressed in LobbyActivity, switching Globals.InLobby to false");
+        Globals.InLobby = false;
+        super.onBackPressed();
     }
 
     public void codeViewClick(View v) {
