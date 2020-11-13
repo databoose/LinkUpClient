6e3dcb615945acdf07f4c9da76bbc523dbc07ca9
data <K1NGKAOS100@Gmail.com>
2020-11-13 06:38:12 -0800

    reformatted code

--- app/src/main/java/com/data/linkup/NetUtils.java
+++ app/src/main/java/com/data/linkup/NetUtils.java
@@ -4,31 +4,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class NetUtils
-{
-    int SendAndWaitReply(String BufferOut, String ExpectedReply, BufferedReader netin, PrintWriter netout)
-    {
-        try
-        {
+public class NetUtils {
+    int SendAndWaitReply(String BufferOut, String ExpectedReply, BufferedReader netin, PrintWriter netout) {
+        try {
             netout.write(BufferOut);
             netout.flush();
-            System.out.println("Sent outwards buffer" + " " + '\"' + BufferOut + '\"' + " " + "to server, waiting for response from server");
+            System.out.println("Sent outwards buffer" + " " + BufferOut + " " + "to server, waiting for response from server");
 
             if (netin.readLine().equals(ExpectedReply)) {
                 System.out.println("Got expected string from server");
                 return 1;
-            }
-
-            else {
+            } else {
                 System.out.println("Receive failure, did not get expected string...");
                 System.out.println("Got : " + netin.readLine());
 
                 return 0;
             }
-        }
-
-        catch (IOException e) { e.printStackTrace();}
-        catch (NullPointerException z) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NullPointerException z) {
             System.out.println("Null pointer exception, most likely the server cut off while we were waiting on readLine()");
             //z.printStackTrace();
         }
