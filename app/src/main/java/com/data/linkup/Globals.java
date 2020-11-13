07c6d1b47bae2fb23e8f86d8d57c0a93feab3281
data <K1NGKAOS100@Gmail.com>
2020-11-13 06:32:23 -0800

    Replaced raw variables in Globals class to Setter calls to easily debug where a variable is modified from and when

--- app/src/main/java/com/data/linkup/Globals.java
+++ app/src/main/java/com/data/linkup/Globals.java
@@ -1,11 +1,42 @@
 package com.data.linkup;
 
-public class Globals
-{
+import android.util.Log;
+
+public class Globals {
     public static String HwidString;
     public static String ConnectCode;
-
     public static Boolean IsVerified;
     public static Boolean InLobby;
     public static Boolean GotConnectCode;
+
+    public static void setHwidString(String callerIdent, String HWID) {
+        HwidString = HWID;
+        Log.d(callerIdent, "set HWID string to :" + HWID);
+    }
+
+    public static void setConnectCode(String callerIdent, String connectcode) {
+        ConnectCode = connectcode;
+        Log.d(callerIdent, "set ConnectCode to : " + connectcode);
+    }
+
+    public static void setIsVerified(String callerIdent, Boolean trueorfalse) {
+        IsVerified = trueorfalse;
+        if(BuildConfig.DEBUG) {
+            Log.d(callerIdent, "set IsVerified to : " + trueorfalse);
+        }
+    }
+
+    public static void setInLobby(String callerIdent, Boolean trueorfalse) {
+        InLobby = trueorfalse;
+        if(BuildConfig.DEBUG) {
+            Log.d(callerIdent, "set InLobby to : " + trueorfalse);
+        }
+    }
+
+    public static void setGotConnectCode(String callerIdent, Boolean trueorfalse) {
+        GotConnectCode = trueorfalse;
+        if(BuildConfig.DEBUG) {
+            Log.d(callerIdent, "set GotConnectCode to : " + trueorfalse);
+        }
+    }
 }
