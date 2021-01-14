3f117b2a747983b3d753bb16b90d6f78770f2d13
data <K1NGKAOS100@Gmail.com>
2021-01-14 04:51:37 -0800

    Buggy implementation of incoming connection check and name input box

--- app/src/main/java/com/data/linkup/LobbyActivity.java
+++ app/src/main/java/com/data/linkup/LobbyActivity.java
@@ -3,7 +3,10 @@ package com.data.linkup;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
@@ -11,6 +14,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LobbyActivity extends AppCompatActivity {
@@ -61,14 +65,66 @@ public class LobbyActivity extends AppCompatActivity {
                     codeInput.setCursorVisible(false);
             }
         });
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    if (Globals.ReceivingConnection == true && Globals.SenderName != null) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LobbyActivity.this);
 
+                                builder.setTitle("Incoming connection");
+                                builder.setMessage("User " + Globals.SenderName + " wants to know your location, accept?");
+
+                                builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+
+                                builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+                                AlertDialog alert = builder.create();
+                                alert.show();
+                            }
+                        });
+                        Globals.setReceivingConnection("LobbyActivity_Main()", false);
+                    }
+                }
+            }
+        });
         //at end
         Globals.setIsVerified("LobbyActivity_Main()", false);
     }
 
     public void btnConnect(View view) {
-        Globals.setTargetCode("btnConnect", codeInput.getText().toString());
-        Globals.setConnecting("btnConnect", true);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Enter name (The recipient sees this)");
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Globals.setName("btnConnect", input.getText().toString());
+                Globals.setTargetCode("btnConnect", codeInput.getText().toString());
+                Globals.setConnecting("btnConnect", true);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
 
     @Override
